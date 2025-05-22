3faff11745dbba18744495c09b4f7cbb61367f58
Sagar Adhikari <sagar@ojastech.io>
2025-05-22 14:37:23 +0545

    Removed auth

--- src/app/services/productsService.ts
+++ src/app/services/productsService.ts
@@ -7,8 +7,8 @@ const createPublicClient = () => {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
     {
       auth: {
-        persistSession: true,
-        autoRefreshToken: true,
+        persistSession: false,
+        autoRefreshToken: false,
       }
     }
   );
